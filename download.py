1febecfe22d69f19da29f5a5e1a49e8c792e292d
mikei72 <2916625750@qq.com>
2025-08-04 00:01:34 +0800

    update

--- download.py
+++ download.py
@@ -4,10 +4,13 @@ import os
 repo_id = "stable-diffusion-v1-5/stable-diffusion-v1-5"
 exclude_files = ["v1-5-pruned-emaonly.ckpt", "v1-5-pruned-emaonly.safetensors",
                  "v1-5-pruned.ckpt", "v1-5-pruned.safetensors",
-                 "model.safetensors", "pytorch_model.bin", "pytorch_model.fp16.bin",
-                 "diffusion_pytorch_model.safetensors", "diffusion_pytorch_model.fp16.bin",
-                 "diffusion_pytorch_model.bin", "diffusion_pytorch_model.non_ema.safetensors",
-                 "diffusion_pytorch_model.non_ema.bin"]
+                 "safety_checker/model.safetensors", "safety_checker/pytorch_model.bin",
+                 "safety_checker/pytorch_model.fp16.bin", "text_encoder/model.safetensors",
+                 "text_encoder/pytorch_model.bin", "text_encoder/pytorch_model.fp16.bin",
+                 "unet/diffusion_pytorch_model.safetensors", "unet/diffusion_pytorch_model.fp16.bin",
+                 "unet/diffusion_pytorch_model.bin", "unet/diffusion_pytorch_model.non_ema.safetensors",
+                 "unet/diffusion_pytorch_model.non_ema.bin", "vae/diffusion_pytorch_model.safetensors",
+                 "vae/diffusion_pytorch_model.fp16.bin", "vae/diffusion_pytorch_model.bin"]
 
 local_dir = "D:/TJU/3.2/IntelliAnalyze/S2-SR/models/stable-diffusion-v1.5"
 
